753bbebc9a64e18ca6d095de3d57af954c5fcea7
shallon <shallonmokeira@gmail.com>
2025-03-30 22:31:49 +0300

    feat: create initial migrations

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -15,8 +15,7 @@ class Category(models.Model):
    return self.name
  
 class Product(models.Model):
- category = models.ForeignKey(Category,
- related_name='products')
+ category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
  name = models.CharField(max_length=200, db_index=True)
  slug = models.SlugField(max_length=200, db_index=True)
  image = models.ImageField(upload_to='products/%Y/%m/%d',
@@ -30,6 +29,8 @@ class Product(models.Model):
 
  class Meta:
   ordering = ('name',)
-  index_together = (('id', 'slug'),)
+  indexes = [
+        models.Index(fields=["id", "slug"]),
+    ]
  def __str__(self):
    return self.name
