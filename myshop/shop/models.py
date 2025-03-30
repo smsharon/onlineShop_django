328383bc59f224c5d9a9ea49fb418ec36612e133
shallon <shallonmokeira@gmail.com>
2025-03-30 22:58:18 +0300

    feat: build catalog views

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 class Category(models.Model):
@@ -13,6 +14,9 @@ class Category(models.Model):
   verbose_name_plural = 'categories'
  def __str__(self):
    return self.name
+ def get_absolute_url(self):
+     return reverse('shop:product_list_by_category',
+                   args=[self.slug])
  
 class Product(models.Model):
  category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
@@ -34,3 +38,6 @@ class Product(models.Model):
     ]
  def __str__(self):
    return self.name
+ def get_absolute_url(self):
+    return reverse('shop:product_detail',
+                  args=[self.id, self.slug])
