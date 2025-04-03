1d7efa3941a2708642bd514a6444a2f3267a60e2
Shallon <shallonmokeira@gmail.com>
2025-04-03 17:12:05 +0300

    fix: update styling

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -9,15 +9,15 @@ class Category(models.Model):
       slug = models.SlugField(max_length=200,
       db_index=True,
       unique=True)
-class Meta:
-  ordering = ('name',)
-  verbose_name = 'category'
-  verbose_name_plural = 'categories'
-  def __str__(self):
-   return self.name
-  def get_absolute_url(self):
-     return reverse('shop:product_list_by_category',
-                   args=[self.slug])
+      class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+      def __str__(self):
+        return self.name
+      def get_absolute_url(self):
+        return reverse('shop:product_list_by_category',
+                        args=[self.slug])
  
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
@@ -32,13 +32,13 @@ class Product(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
-class Meta:
-    ordering = ('name',)
-    indexes = [
-          models.Index(fields=["id", "slug"]),
-      ]
-def __str__(self):
-   return self.name
-def get_absolute_url(self):
-    return reverse('shop:product_detail',
-                  args=[self.id, self.slug])
+    class Meta:
+        ordering = ('name',)
+        indexes = [
+              models.Index(fields=["id", "slug"]),
+          ]
+    def __str__(self):
+      return self.name
+    def get_absolute_url(self):
+        return reverse('shop:product_detail',
+                      args=[self.id, self.slug])
--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -16,11 +16,11 @@ def product_list(request, category_slug=None):
 
 def product_detail(request, id, slug):
  product = get_object_or_404(Product,
- id=id,
- slug=slug,
- available=True)
+                            id=id,
+                            slug=slug,
+                            available=True)
  return render(request,
- 'shop/product/detail.html',
- {'product': product}) 
+              'shop/product/detail.html',
+              {'product': product}) 
 
 
