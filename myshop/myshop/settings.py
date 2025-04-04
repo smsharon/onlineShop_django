937a76bb3b2ad3f77038659d2e72ae84d0595ed1
Shallon <shallonmokeira@gmail.com>
2025-04-04 11:18:56 +0300

    feat: create app for managing carts

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+CART_SESSION_ID = 'cart'
