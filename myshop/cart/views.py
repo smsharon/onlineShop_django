44c012ff127d3775854ac2f053a1e4cdbdc52769
Shallon <shallonmokeira@gmail.com>
2025-04-04 17:25:23 +0300

    fix: update shopping cart

--- myshop/cart/views.py
+++ myshop/cart/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-
-# Create your views here.
+from cart.forms import CartAddProductForm
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
 from shop.models import Product
@@ -31,4 +30,15 @@ def cart_remove(request, product_id):
 def cart_detail(request):
     cart = Cart(request)
     return render(request, 'cart/detail.html', {'cart': cart})
+
+#view to add an Add to cart button
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id,
+                                         slug=slug, 
+                                         available=True)
+    cart_product_form = CartAddProductForm()
+    return render(request,
+                  'shop/product/detail.html',
+                  {'product': product,
+                  'cart_product_form': cart_product_form})
     
